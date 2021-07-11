f0287418e32dcce6ad711af229dd188155bced26
Karolina Pałka <karolina.palka.programista@gmail.com>
2021-07-11 17:16:10 +0200

    Create background, paddles and a ball

--- Unit1.h
+++ Unit1.h
@@ -7,10 +7,16 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TShape *Shape1;
+        TImage *paddle;
+        TImage *Image1;
+        TImage *Image2;
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
